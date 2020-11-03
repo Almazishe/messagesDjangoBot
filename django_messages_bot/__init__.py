913e50bbc85b211048a69f673be03629d59ed5ee
Almazishe <mr.esenyazov@gmail.com>
2020-11-04 05:22:02 +0600

    WEBHOOKs

--- django_messages_bot/__init__.py
+++ django_messages_bot/__init__.py
@@ -1,3 +1,3 @@
 # Do not remove these 2 lines:
-bot_token = '1424866327:AAEwZIb17VMQw-8wthhc9BWFn5cQCISKxW8'
+bot_token = '1490862295:AAHGXsPkRqS148toZA4bHu3RcZBKPjj4l5A'
 app_name='django_messages_bot'
